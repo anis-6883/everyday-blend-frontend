55936de01061daf2fdc9cfef31f1f9e2d8bddf17
Muhammad Anisuzzaman <anisseam238@gmail.com>
2023-10-24 22:44:22 +0600

    Change Env Name

--- src/helpers/getAxiosInstances.js
+++ src/helpers/getAxiosInstances.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const quizCraftBackend = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   timeout: 15000,
   headers: {
     "content-type": "application/json",
